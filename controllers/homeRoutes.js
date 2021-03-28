9bc79ae7721c94720441b9159b5a62fea71fe6f5
Jesse Willig <jessewillig@outlook.com>
2021-03-28 12:06:45 -0700

    added get 1 post to routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,4 +24,26 @@ router.get('/', (req, res) => {
     }
 });
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+            ],
+        });
+
+        const post = postData.map((post) => post.get({ plain: true }));
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
