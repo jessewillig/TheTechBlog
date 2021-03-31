c11609c48befb29e7731ba46dd0dc16bb15f3cfb
Jesse Willig <jessewillig@outlook.com>
2021-03-30 21:42:03 -0700

    changed small syntax issues with index.js in models

--- models/index.js
+++ models/index.js
@@ -3,30 +3,30 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE"
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-    foreignKey: "user_id"
-});
-
-Post.hasMany(Comment, {
-    foreignKey: "parent_id",
-    onDelete: "CASCADE"
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: "parent_id"
-});
-
-User.hasMany(Comment, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE"
+    foreignKey: 'user_id'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: "user_id"
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'parent_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
 module.exports = { User, Post, Comment };
